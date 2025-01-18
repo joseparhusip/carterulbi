45e3a5ef79261293888ffb210a80c50638aef8cf
joseparhusip <you@example.com>
2025-01-18 16:16:49 +0700

    Update update_quantity.php
    edit

--- CarterULBI/update_quantity.php
+++ CarterULBI/update_quantity.php
@@ -10,10 +10,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $id_keranjang = $id_keranjang_array[$i];  
         $quantity = $quantity_array[$i];  
   
-        // Update quantity in the database  
-        $sql_update = "UPDATE keranjang SET quantity = ? WHERE id_keranjang = ?";  
+        // Get the price for the current item  
+        $sql_price = "SELECT harga FROM keranjang WHERE id_keranjang = ?";  
+        $stmt_price = $koneksi->prepare($sql_price);  
+        $stmt_price->bind_param("i", $id_keranjang);  
+        $stmt_price->execute();  
+        $result_price = $stmt_price->get_result();  
+        $row_price = $result_price->fetch_assoc();  
+        $harga = $row_price['harga'];  
+  
+        // Calculate total price  
+        $total_harga = $harga * $quantity;  
+  
+        // Update quantity and total_harga in the database  
+        $sql_update = "UPDATE keranjang SET quantity = ?, total_harga = ? WHERE id_keranjang = ?";  
         $stmt_update = $koneksi->prepare($sql_update);  
-        $stmt_update->bind_param("ii", $quantity, $id_keranjang);  
+        $stmt_update->bind_param("iii", $quantity, $total_harga, $id_keranjang);  
         $stmt_update->execute();  
     }  
   
